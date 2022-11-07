b2df5d8a7c406627c2aefee82c9de0ad48c59845
Søren Fuglede Jørgensen <github@fuglede.dk>
2022-11-07 19:58:56 +0100

    Add a missing comma

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/photocopy-wasm' : '/'
 })
