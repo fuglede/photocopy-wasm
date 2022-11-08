8622234ddbd7650506c072339e77e6e88ed36b07
Søren Fuglede Jørgensen <github@fuglede.dk>
2022-11-08 09:53:36 +0100

    Remove repository name from path, since there's a custom domain in place now

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/photocopy-wasm' : '/'
+  transpileDependencies: true 
 })
